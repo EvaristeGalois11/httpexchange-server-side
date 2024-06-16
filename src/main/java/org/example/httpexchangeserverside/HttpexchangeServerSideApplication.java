045b6edc7fd2123a118740d90d45689d5a7d646c
Claudio Nave <evaristegalois11@proton.me>
2024-06-16 22:35:27 +0200

    Init project

---
+++ src/main/java/org/example/httpexchangeserverside/HttpexchangeServerSideApplication.java
@@ -0,0 +1,13 @@
+package org.example.httpexchangeserverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpexchangeServerSideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpexchangeServerSideApplication.class, args);
+    }
+
+}
